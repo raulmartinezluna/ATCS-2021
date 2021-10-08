0ea5cbd340fb297aeb22edfaccc25999fedd9bca
Raul Martinez Luna <raul.martinez.luna.2005@gmail.com>
2021-10-08 11:46:29 -0700

    Add 3rd Commit

---
+++ project0/addCalc.py
@@ -0,0 +1,7 @@
+#Write a function that takes in two numbers, and adds them together. Make your function print out a sentence showing the two numbers, and the result.
+
+def add_print(num1, num2):
+    print(str(num1), " + ", str(num2), " = ", str(num1+num2))
+
+#Call your function with three different sets of numbers.
+add_print(1, 2)
