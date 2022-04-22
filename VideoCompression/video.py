79fe0661d965205e321e5aa966a075373c71e656
Raul Martinez Luna <raul.martinez.luna.2005@gmail.com>
2022-04-22 14:58:30 -0700

    add takeVideo()

--- VideoCompression/video.py
+++ VideoCompression/video.py
@@ -12,5 +12,31 @@ import os
 
 ''' Object handles video recording '''
 class VideoRecorder:
+    '''
+        Function takes in cv and returns a tuple of retList & frameList.
+        ret is a boolean that says whether it's accompanying frame is usable.
+        (ret makes up retList & frame makes up frameList)
+        '''
+
+    def takeVideo(self):
+
+        ''' Code below is partially from https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/ '''
+
+        webcam = cv.VideoCapture(0)
+        retList = []
+        frameList = []
+
+        while (True):
+            ret, frame = webcam.read()
+            cv.imshow('Recorder', frame)
+            retList.append(ret)
+            frameList.append(frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+        webcam.release()
+        cv.destroyAllWindows()
+
+        ''' Code above is partially from https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/ '''
+        return zip(retList, frameList)
 
 if __name__ == '__main__':
