a31df59bac15e6a992b2101e36262c758e65ec79
Raul Martinez Luna <raul.martinez.luna.2005@gmail.com>
2022-04-22 14:49:25 -0700

    add program description

---
+++ VideoCompression/video.py
@@ -0,0 +1,8 @@
+'''
+Program: Records a video and provides an original version and a compressed version
+Teacher: Nandhini Namasivayam
+Student Author: Raul Martinez Luna
+Started: April 22nd, 2020
+Ended:
+
+'''
