e170b7b703066f26817a8076f8d4019488fcddcb
Raul Martinez Luna <raul.martinez.luna.2005@gmail.com>
2022-05-02 09:19:36 -0700

    change create() to make two videos (Original & Compressed)

--- VideoCompression/video.py
+++ VideoCompression/video.py
@@ -1,38 +1,47 @@
 import os
-import cv2
-
-''' Altered from https://www.codingforentrepreneurs.com/blog/how-to-record-video-in-opencv-python '''
+import cv2 as cv
 
 class Video:
 
-    filename = 'Video/video.mp4'
+    filenameOriginal = 'Video/videoOriginal.mp4'
+    filenameCompressed = 'Video/videoCompressed.mp4'
     framesPerSecond = 24.0
     resolution = (1280, 720)
-    def __init__(self, fn, fps, w, h):
-        filename = fn
-        framesPerSecond = fps
-        resolution = (w, h)
+
+    def __init__(self, fnO, fnC, fps, w, h):
+        self.filenameOriginal = fnO
+        self.filenameCompressed = fnC
+        self.framesPerSecond = fps
+        self.resolution = (w, h)
 
     def create(self):
+        ''' Altered from https://www.codingforentrepreneurs.com/blog/how-to-record-video-in-opencv-python '''
         width = self.resolution[0]
         height = self.resolution[1]
-        cap = cv2.VideoCapture(0)
+        cap = cv.VideoCapture(0)
         cap.set(3, width)
         cap.set(4, height)
-        out = cv2.VideoWriter(self.filename, cv2.VideoWriter_fourcc(*'XVID'), 25, (width, height))
+        videoOriginal = cv.VideoWriter(self.filenameOriginal, cv.VideoWriter_fourcc(*'XVID'), 25, (width, height))
+        videoCompressed = cv.VideoWriter(self.filenameCompressed, cv.VideoWriter_fourcc(*'XVID'), 25, (width, height))
+
 
         while True:
             ret, frame = cap.read()
-            out.write(frame)
-            print(frame)
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            videoOriginal.write(frame)
+            #Alter frames right here
+
+            videoCompressed.write(frame)
+            #Alter frames right here
+            cv.imshow('frame', frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
                 break
 
         cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+        videoOriginal.release()
+        videoCompressed.release()
+        cv.destroyAllWindows()
+        ''' Altered from https://www.codingforentrepreneurs.com/blog/how-to-record-video-in-opencv-python '''
 
 if __name__ == '__main__':
-    video = Video('Video/video.mp4', 24.0, 1280, 720)
+    video = Video('Video/videoOriginal.mp4', 'Video/videoCompressed.mp4', 24.0, 1280, 720)
     video.create()
